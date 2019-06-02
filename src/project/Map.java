034d1e8f72d9f2b6cfd02be928167a2f769f31d1
Szado12 <michal.pijanowski2@gmail.com>
2019-06-02 17:42:54 +0200

    adding 2nd constructor

--- src/project/Map.java
+++ src/project/Map.java
@@ -10,6 +10,69 @@ public class Map extends MapInformationsAbstract  {
 	IMarsRover rover = new MarsRover();
 	IColony colony = new Colony(10);
 
+	
+	
+	void creating(int numOfGenW,int numOfGenO,int numOfGenF,int n, int m) {
+		
+		water= new Resources[numOfGenW];
+		oxygen= new Resources[numOfGenO];
+		food = new Resources[numOfGenF];
+	
+	mapa = new String[n][m];
+	day = 1;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			if(i==0 || i==n-1) mapa[i][j]="─";
+			if(j==0 || j==m-1) mapa[i][j]="│";
+			if(i!=0 && j!=0 && i!=n-1 && j!=m-1) mapa[i][j]=" ";
+		}
+	}
+	
+	mapa[0][0]="┌";        //skrajne punkty ramki
+	mapa[n-1][m-1]="┘";
+	mapa[0][m-1]="┐";
+	mapa[n-1][0]="└";
+	
+	rover.setX(RandomCoord.getRandX(this));
+	rover.setY(RandomCoord.getRandY(this));
+	mapa[rover.getX()][rover.getY()]= rover.getAppearance();
+	
+	for(int i=0;i<numOfGenW;i++) 
+	{
+		water[i] = new Resources();  //trzeba zainicjowac kazdy obiekt w talblicy, cholernie WAZNE !!!
+		water[i].setAppearance("W");
+		water[i].setX(RandomCoord.getRandX(this));
+		water[i].setY(RandomCoord.getRandY(this));
+		mapa[water[i].getX()][water[i].getY()] = water[i].getAppearance();
+	}
+	
+	for(int i=0;i<numOfGenO;i++)  
+	{
+		oxygen[i] = new Resources(); 
+		oxygen[i].setAppearance ("O");
+		oxygen[i].setX(RandomCoord.getRandX(this));
+		oxygen[i].setY(RandomCoord.getRandY(this));
+		mapa[oxygen[i].getX()][oxygen[i].getY()] = oxygen[i].getAppearance();
+	}
+	
+	for(int i=0;i<numOfGenF;i++)  
+	{
+		food[i] = new Resources(); 
+		food[i].setAppearance("F");
+		food[i].setX(RandomCoord.getRandX(this));
+		food[i].setY(RandomCoord.getRandY(this)); 
+		mapa[food[i].getX()][food[i].getY()] = food[i].getAppearance();
+	}
+}
+	
+	
+	
+	
+	
+	
+	
 	public Map()                                //domyslny konstruktor, bez parametrow
 	{	
 		numOfGenO = 20;
@@ -22,70 +85,35 @@ public class Map extends MapInformationsAbstract  {
 		necessaryF = 3;
 		necessaryW = 7;
 		
-		water= new Resources[numOfGenW];
-		oxygen= new Resources[numOfGenO];
-		food = new Resources[numOfGenF];
-		mapa = new String[100][100];
+		
+		
 		n=20;
 		m=50;
-		day = 1;
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<m;j++)
-			{
-				if(i==0 || i==n-1) mapa[i][j]="─";
-				if(j==0 || j==m-1) mapa[i][j]="│";
-				if(i!=0 && j!=0 && i!=n-1 && j!=m-1) mapa[i][j]=" ";
-			}
-		}
+		creating(numOfGenW, numOfGenO,numOfGenF,n,m);
 		
-		mapa[0][0]="┌";        //skrajne punkty ramki
-		mapa[n-1][m-1]="┘";
-		mapa[0][m-1]="┐";
-		mapa[n-1][0]="└";
-		
-		rover.setX(RandomCoord.getRandX(this));
-		rover.setY(RandomCoord.getRandY(this));
-		mapa[rover.getX()][rover.getY()]= rover.getAppearance();
-		
-		for(int i=0;i<numOfGenW;i++) 
-		{
-			water[i] = new Resources();  //trzeba zainicjowac kazdy obiekt w talblicy, cholernie WAZNE !!!
-			water[i].setAppearance("W");
-			water[i].setX(RandomCoord.getRandX(this));
-			water[i].setY(RandomCoord.getRandY(this));
-			mapa[water[i].getX()][water[i].getY()] = water[i].getAppearance();
-		}
-		
-		for(int i=0;i<numOfGenO;i++)  
-		{
-			oxygen[i] = new Resources(); 
-			oxygen[i].setAppearance ("O");
-			oxygen[i].setX(RandomCoord.getRandX(this));
-			oxygen[i].setY(RandomCoord.getRandY(this));
-			mapa[oxygen[i].getX()][oxygen[i].getY()] = oxygen[i].getAppearance();
-		}
-		
-		for(int i=0;i<numOfGenF;i++)  
-		{
-			food[i] = new Resources(); 
-			food[i].setAppearance("F");
-			food[i].setX(RandomCoord.getRandX(this));
-			food[i].setY(RandomCoord.getRandY(this)); 
-			mapa[food[i].getX()][food[i].getY()] = food[i].getAppearance();
-		}
 	}
-	
-	public Map(int n, int m /*...*/)                     //konstruktor z podanymi parametrami
+	public Map(int numOfGenW, int numOfGenO, int numOfGenF, int necessaryW, int necessaryO, int necessaryF, int n, int m)                     //konstruktor z podanymi parametrami
 	{
-		//TODO ...
+		this.numOfGenO = numOfGenO;
+		this.numOfGenF = numOfGenF;
+		this.numOfGenW = numOfGenW;
+		collectedO = 0;
+		collectedF = 0;
+		collectedW = 0;
+		this.necessaryO = necessaryO;
+		this.necessaryF = necessaryF;
+		this.necessaryW = necessaryW;
+		this.n=n;
+		this.m=m;
+		creating(numOfGenW, numOfGenO,numOfGenF,n,m);
+		
 	}
 	
 	public  void mapOutput()
 	{
-		for(int i=0;i<n;i++)
+		for(int i=0;i<this.n;i++)
 		{
-			for(int j=0;j<m;j++)
+			for(int j=0;j<this.m;j++)
 			{
 				System.out.print(mapa[i][j]);
 			}

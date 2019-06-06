10aeb2c74c403ac5a88590884efff970ac66a3dc
Jakub <jakubtolsciuk@gmail.com>
2019-06-07 00:14:56 +0200

    Added unit tests

---
+++ src/project/RoverTest.java
@@ -0,0 +1,52 @@
+package project;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RoverTest {
+	
+	@Test
+	public void marsRoverFuelTest() {
+		MarsRover marsRover = new MarsRover();
+		int fuel = marsRover.getFuel();
+		assertEquals(100, fuel);		
+	}
+	
+	@Test
+	public void marsRoverBatteryStatusTest() {
+		MarsRover marsRover = new MarsRover();
+		int batteryStatus = marsRover.getBatteryStatus();
+		assertEquals(0, batteryStatus);		
+	}
+	
+	@Test
+	public void marsRoverChargingTest() {
+		MarsRover marsRover = new MarsRover();
+		boolean charging = marsRover.isCharging();
+		assertEquals(false, charging);		
+	}
+	
+	@Test
+	public void marsRoverMovingTest() {
+		Map map = new Map();
+		MarsRover marsRover = new MarsRover();
+		
+		
+		while (marsRover.getFuel() != 0)
+		try {
+			marsRover.makeMove(map);
+		} catch (Exception e) {
+		}
+		
+		marsRover.checkStatus(map);
+		
+		int batteryStatus = marsRover.getBatteryStatus();
+		assertEquals(5, batteryStatus);	
+		
+		boolean charging = marsRover.isCharging();
+		assertEquals(true, charging);	
+		
+	}
+
+}
